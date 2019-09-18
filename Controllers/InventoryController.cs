a9a8766e8a0f282e4bf34e5aeacfd1a0f4f12ac4
ads6495 <astrange55@icloud.com>
2019-09-18 14:41:04 -0400

    First 2 done

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using storeproject;
@@ -7,21 +8,35 @@ namespace StoreProject.Controllers
 {
   [Route("api[controller]")]
   [ApiController]
-  public class InventoryController { }
-  //   {
-  //     private DatabaseContext context;
+  public class InventoryController : ControllerBase
+  {
+    private DatabaseContext context;
 
-  //     public InventoryController()
-  //     {
-  //       this.context = new DatabaseContext();
-  //     }
+    public InventoryController()
+    {
+      this.context = new DatabaseContext();
+    }
 
-  //     [HttpPost]
-  //     public ActionResult<IOrderedEnumerable<Inventory>> GetAllInventory()
-  //     {
-  //       var Invetories = context.Inventories;
-  //       context.SaveChanges();
-  //     }
+    [HttpGet]
+    public ActionResult<IEnumerable<Inventory>> GetAllInventory()
+    {
+      var invs = context.Inventories.OrderByDescending(inv => inv.SKU);
 
-  //   }
+      return invs.ToList();
+    }
+
+    [HttpGet("{Id}")]
+    public ActionResult GetOneItem(int Id)
+    {
+      var invs = context.Inventories.FirstOrDefault(inv => inv.Id == Id);
+      if (invs != null)
+      {
+        return Ok(invs);
+      }
+      else
+      {
+        return NotFound();
+      }
+    }
+  }
 }
